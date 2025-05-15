a66b57e916cf16ec394db7fc48e70fce4e08f3dd
chabuuuu <haphuthinh332004@gmail.com>
2025-05-15 23:26:02 +0700

    feat(email): using custom name

--- src/service/email.service.ts
+++ src/service/email.service.ts
@@ -6,8 +6,9 @@ import { injectable } from 'inversify';
 @injectable()
 export class EmailService implements IEmailService {
   async sendEmail(sendEmailReq: SendEmailReq): Promise<void> {
+    const name = sendEmailReq.from.name ? sendEmailReq.from.name : 'GiaSuVLU';
     await sendEmail({
-      from: { name: 'GiaSuVLU' },
+      from: { name: name },
       to: { emailAddress: sendEmailReq.to.emailAddresses },
       subject: sendEmailReq.subject,
       html: sendEmailReq.html
